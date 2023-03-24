9ea171d9d62e62f1de481a538d2921dc06cf2656
ChandanPk <chandanpk9@gmail.com>
2023-03-24 21:03:48 +0530

    add styles for add-blog page

--- client/src/comps/AddBlog.js
+++ client/src/comps/AddBlog.js
@@ -1,48 +1,57 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 // import handlePost from '../hooks/post';
-import { API } from '../constants';
-import { useHistory } from 'react-router';
-
+import { API } from "../constants";
+import { useHistory } from "react-router";
 
 const AddBlog = () => {
+  const { register, handleSubmit } = useForm();
+  const history = useHistory();
 
-    const {register, handleSubmit} = useForm();
-    const history = useHistory();
+  const onSubmit = (data) => {
+    const { title, author, body } = data;
+    const id = Math.random();
 
+    console.log(title, author, body);
+    fetch(API.blogs, {
+      method: "POST",
+      body: JSON.stringify({ title, author, body, id }),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        data.success && history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const onSubmit = (data) => {
-        const {title, author, body} = data
-        const id = Math.random();
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <div className="add-blog-header">Add blog</div>
+        <input
+          className="title-field field"
+          type="text"
+          placeholder="title"
+          {...register("title", { required: true })}
+        />
+        <input
+          className="author-field field"
+          type="text"
+          placeholder="author"
+          {...register("author", { required: true })}
+        />
+        <textarea
+          className="body-field field"
+          type="text"
+          placeholder="body"
+          {...register("body", { required: true })}
+        />
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-        console.log(title, author, body)
-        fetch(API.blogs, {
-            method: "POST",
-            body: JSON.stringify({title, author, body, id}),
-            headers: {"Content-type": "application/json"}
-        })
-
-        .then(res => res.json())
-            .then(data => {
-                data.success && history.push('/')
-            })
-                .catch(err => {
-                    console.log(err)
-                })
-    }
-
-
-    return ( 
-        <div>
-        <div>Add blogs</div>
-        <form onSubmit={handleSubmit(onSubmit)} >
-            <input className="title-field" type="text" placeholder="title" {...register("title", {required: true})} />
-            <input className="author-field" type="text" placeholder="author" {...register('author', {required: true})} /> 
-            <input className="body-field" type="text" placeholder="body" {...register('body', {required: true})} /> 
-            <button>Submit</button>
-        </form>
-
-        </div>
-     );
-}
- 
 export default AddBlog;
