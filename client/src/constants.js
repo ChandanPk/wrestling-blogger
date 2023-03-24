e4990346584af90a872c40defe0955939de89657
ChandanPk <chandanpk9@gmail.com>
2023-03-24 19:38:57 +0530

    resolve db connection issues

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,8 +1,9 @@
-// export const BASE_URL = "https://my-json-server.typicode.com/ChandanPk/json-placeholder-api"
-export const BASE_URL = "http://localhost:8001"
+// export const BASE_URL =
+//   "https://my-json-server.typicode.com/ChandanPk/json-placeholder-api";
+export const BASE_URL = "http://localhost:8001";
 
 export const API = {
-    blogs:  BASE_URL + "/blogs",
-}
+  blogs: BASE_URL + "/blogs",
+};
 
-API.blogPage = (id) => BASE_URL + "/blogs/" + id
+API.blogPage = (id) => BASE_URL + "/blogs/" + id;
